a2d877f3009a0b773b8412cd760a54d59d5323f0
Nasrin Raha <59247592+Nasrin-Raha@users.noreply.github.com>
2023-10-12 13:48:21 +0200

    Add files via upload

---
+++ sample2.py
@@ -0,0 +1,21 @@
+
+"""
+This is a simple Python script that adds two numbers and prints the result.
+"""
+
+def add(number1, number2):
+    """
+    Add two numbers and return the result.
+
+    :param number1: The first number.
+    :param number2: The second number.
+    :return: The sum of number1 and number2.
+    """
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
+
+
